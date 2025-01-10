2421fe85295ec0e0ae60ed5b9ecf92cba8685946
Lauviah2024 <lauviah.vlavonou@epitech.eu>
2025-01-10 15:26:52 +0100

    Oauth with codeIgniter

---
+++ app/Models/BlogModel.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BlogModel extends Model{
+    protected $table = 'blog';
+    protected $primarykey = 'post_id';
+    protected $allowFields = ['post_title', 'post_description'];
+}
